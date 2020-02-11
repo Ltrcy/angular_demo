7cb1f7fda5161ddcc208bd0b1b60f5701894376b
Ltrcy <1214506611@qq.com>
2020-02-11 12:04:12 +0800

    angular-demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+// import { SearchComponent } from './components/search/search.component';
+// import { FormComponent } from './components/form/form.component';
+
+// 引入表单相关的模块，才可以用双向数据绑定
+import { FormsModule } from '@angular/forms';
+import { TodolistComponent } from './components/todolist/todolist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodolistComponent,
+    // SearchComponent
+    // FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
